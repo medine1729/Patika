56e761e7848f3321d8108ff48df238f747814ff3
Medine Kaynak <151500611+medine1729@users.noreply.github.com>
2024-08-04 11:37:48 +0300

    Add files via upload

---
+++ #patikaproject.py
@@ -0,0 +1,26 @@
+#patikaproject
+#[22,27,16,2,18,6]n -> Insertion Sort
+
+#Yukarı verilen dizinin sort türüne göre aşamalarını yazınız.
+[16,22,27,2,18,6]n-1
+[2,16,22,27,18,6]n-2
+[2,16,18,22,27,6]n-3
+[2,6,16,18,22,27]1
+#Big-O gösterimini yazınız.
+2^x = n , x= logn O(logn)
+
+#Time Complexity: Dizi sıralandıktan sonra 18 sayısı aşağıdaki case'lerden hangisinin kapsamına girer? Yazınız
+-Average Case
+#Average case: Aradığımız sayının ortada olması
+#Worst case: Aradığımız sayının sonda olması
+#Best case: Aradığımız sayının dizinin en başında olması."""
+.
+
+
+
+#[7,3,5,8,2,9,4,15,6] dizisinin Selection Sort'a göre ilk 4 adımını yazınız.
+[3,7,5,8,2,9,4,15,6]
+[3,5,7,8,2,9,4,15,6]
+[2,3,5,7,8,9,4,15,6]
+[2,3,4,5,7,8,9,15,6]
+
